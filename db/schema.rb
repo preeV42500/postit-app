1fe4a1e06b8659870383684669745881933b80ff
Preeti Viswanathan <vis.preeti@gmail.com>
2017-09-21 19:53:14 -0700

    set up many to many association between post and category models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922014036) do
+ActiveRecord::Schema.define(version: 20170922022446) do
 
   create_table "categories", force: true do |t|
     t.text "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170922014036) do
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "category_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
