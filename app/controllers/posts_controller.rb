88e23937ac64ee57d249e2233a576417f0647f92
Preeti Viswanathan <vis.preeti@gmail.com>
2017-09-23 21:23:47 -0700

    create model backed form for new category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,28 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = User.first #Todo: change once we have authentication
 
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,6 +30,22 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
